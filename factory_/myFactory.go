9c7c87e8925479bc5d8777f1186376a3d0b25bc0
Viktor-sv <viktor.stegachov@gmail.com>
2021-11-11 15:23:08 +0200

    fix changes

--- factory_/myFactory.go
+++ factory_/myFactory.go
@@ -5,58 +5,57 @@ import (
 	"time"
 )
 
-type myIntreface interface{
+type myIntreface interface {
 	Run(time time.Duration) error
 }
 
-type worker  struct {
+type worker struct {
 	name string
 }
 
-func (s * worker) Error() string {
+func (s *worker) Error() string {
 	return ""
 }
-func (s * worker) String() string {
+func (s *worker) String() string {
 	return ""
 }
 
-func (s *worker ) Run(duration time.Duration) error {
+func (s *worker) Run(duration time.Duration) error {
 	fmt.Printf("worker run \n")
 	time.Sleep(duration)
 	fmt.Println(s.name)
 	return nil
 }
 
-func (s *worker) myfucn(i int) error{
+func (s *worker) myfucn(i int) error {
 	return s
 }
 
-type  DataWriter interface{
+type DataWriter interface {
 	WriteTo(string2 string)
 }
 
 type Map struct {
-	size int
+	size    int
 	storage map[string]string
 }
 
 type DB struct {
-
 }
 
-func (m *Map) WriteTo(string2 string){
+func (m *Map) WriteTo(string2 string) {
 
 	m.storage["k"] = string2
 
 }
 
-func (m *DB) WriteTo(string2 string){
+func (m *DB) WriteTo(string2 string) {
 	//connect to DB
 	//write to db
 
 }
 
-func do(w myIntreface){
+func do(w myIntreface) {
 
 }
 
@@ -64,7 +63,7 @@ type dd int
 
 func (i dd) Run(duration time.Duration) error {
 	fmt.Printf("dd run \n")
-	time.Sleep(time.Duration(i)*time.Second)
+	time.Sleep(time.Duration(i) * time.Second)
 	return nil
 }
 
@@ -74,8 +73,12 @@ func Factory(t string) DataWriter {
 	case "DB":
 		return &DB{}
 	case "MAP":
-		return &Map{storage:make(map[string]string)}
+		return &Map{storage: make(map[string]string)}
 	}
 
 	return nil
 }
+
+//example of using
+//f := factory_.Factory("DB")
+//f.WriteTo("hollo")
--- main.go
+++ main.go
@@ -11,12 +11,9 @@ import (
 	"log"
 	"net/http"
 	_ "os"
-	"reflect"
 	"strconv"
 	"strings"
-
 	"time"
-	"unsafe"
 )
 
 type Config struct {
@@ -48,105 +45,18 @@ type Token struct {
 }
 
 const (
-	configFile string = "config/config.json"
-	host       string = "127.0.0.1"
+	configFile    string = "config/config.json"
+	host          string = "127.0.0.1"
+	my_token_pass string = "paswdwdwdwdwdwdwd"
 )
 
 var events = make(map[string]Event, 1000)
 var users = make([]User, 0, 10000)
 
-type jsonPersarer interface {
-	Parse(b []byte) error
-}
-
-func (e *Event) Parse(b []byte) error {
-
-	err := json.Unmarshal(b, e)
-	return err
-}
-
-func (e *User) Parse(b []byte) error {
-
-	err := json.Unmarshal(b, e)
-	return err
-}
-
-type id string
-
-func (i *id) Parse(b []byte) error {
-
-	err := json.Unmarshal(b, i)
-	return err
-}
-func parseReq(req io.Reader, v jsonPersarer) error {
-	body, err := ioutil.ReadAll(req)
-	if err != nil {
-		return err
-	}
-
-	err = v.Parse(body)
-	return err
-}
-
-func response(w http.ResponseWriter, req *http.Request) string {
-	return "OK"
-}
-
-// a handler is by using the `http.HandlerFunc` adapter
-// on functions with the appropriate signature.
-func hello(w http.ResponseWriter, req *http.Request) {
-	log.Println(req.Host)
-	fmt.Fprintf(w, "hello my tyest \n")
-}
-
-func loginHandler(w http.ResponseWriter, req *http.Request) {
-	u := User{}
-	parseReq(req.Body, &u)
-	fmt.Println(len(users))
-
-	u.Token = genToken(&u)
-	b, err := json.Marshal(u)
-	if err != nil {
-		fmt.Fprintf(w, "err %s \n", err.Error())
-		return
-	}
-
-	w.Header().Add("Content-Type", " application/json")
-	fmt.Fprintf(w, string(b))
-
-	//fmt.Fprintf(w, "User {%s} need to create an account firt. \n", u.Name)
-	return
-}
-
 func removeElement(u []User, idx int) []User {
 	return append(u[:idx], u[idx+1:]...)
 }
 
-func logoutHandler(w http.ResponseWriter, req *http.Request) {
-	u := User{}
-	err := parseReq(req.Body, &u)
-	if err != nil {
-		fmt.Fprintf(w, "Bad data \n")
-	}
-
-	for idx, v := range users {
-		fmt.Println("idx", idx, "v name: ", v.Name, "uname: ", u.Name)
-		if v.Name == u.Name {
-			//delete  from map
-			removeElement(users, idx)
-			fmt.Fprintf(w, "User {%s} logged out \n", u.Name)
-			return
-		}
-
-		fmt.Fprintf(w, "User {%s} was not logged in. \n", u.Name)
-		return
-	}
-
-	fmt.Fprintf(w, "User {%s} was not logged in. \n", u.Name)
-	return
-}
-
-// localTime return time based on location
 func localTime(location string) int {
 	loc, err := time.LoadLocation("America/Chicago")
 	if err != nil {
@@ -159,161 +69,10 @@ func localTime(location string) int {
 	return 0
 }
 
-func eventsHandler(w http.ResponseWriter, req *http.Request) {
-	body, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	e := Event{}
-	err = json.Unmarshal(body, &e)
-	if err != nil {
-		fmt.Fprintf(w, "Unmarshal err %s \n", err.Error())
-		return
-	}
-
-	fmt.Println(string(body))
-
-	if !userLoggedIn(e.Name) {
-		fmt.Fprintf(w, "User was not logged in %s \n", e.Name)
-		return
-	}
-
-	_, found := events[e.Id]
-	if found {
-		events[e.Id] = e
-	}
-
-	return
-}
-
-//eventHandler process req  from ...
-func getEventsHandler(w http.ResponseWriter, req *http.Request) {
-	var eventId id
-	parseReq(req.Body, &eventId)
-
-	eVents := make(map[string]Event)
-
-	for k, v := range events {
-		if v.Id == string(eventId) {
-			eVents[k] = v
-		}
-	}
-
-	fmt.Fprintf(w, "Num of events {%d}  \n", len(eVents))
-}
-
-func postEventsHandler(w http.ResponseWriter, req *http.Request) {
-
-}
-
-func putEventsHandler(w http.ResponseWriter, request *http.Request) {
-
-	c := context.WithValue(request.Context(), "id", "user_id")
-
-	request.WithContext(c)
-
-	//context.WithTimeout(c, (2 * time.Duration())
-	//updateDB(c, data))
-	v := request.Context().Value("w")
-	if v != nil {
-
-		s := v.(string)
-		fmt.Println(s)
-	}
-
-}
-
-func getSliceHeader(slice *[]int) string {
-	sh := (*reflect.SliceHeader)(unsafe.Pointer(slice))
-	return fmt.Sprintf("%+v", sh)
-}
-
-func registerHandlers() *mux.Router {
-	r := mux.NewRouter()
-
-	//user PUT
-	r.HandleFunc("/login", loginHandler)
-	r.HandleFunc("/logout", auth(logoutHandler))
-
-	//user PUT
-	r.HandleFunc("/api/put", auth(eventsHandler))
-
-	//events GET PUT POST
-	r.HandleFunc("/api/events", auth(getEventsHandler)).Methods(http.MethodGet)
-	r.HandleFunc("/api/events", auth(postEventsHandler)).Methods(http.MethodPost)
-	r.HandleFunc("/api/events", auth(putEventsHandler)).Methods(http.MethodPut)
-
-	return r
-}
-
 func intiLog() {
 	fmt.Println("Hello world!", localTime("Chernivtsi"))
+
 }
-
-func userHandler(w http.ResponseWriter, req *http.Request) {
-
-	body, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	u := User{}
-	json.Unmarshal(body, &u)
-
-	fmt.Println(string(body))
-
-	if !userLoggedIn(u.Name) {
-		fmt.Fprintf(w, "User {%s} not logged in. \n", u.Name)
-	}
-
-	//todo update events
-	return
-}
-
-func parseToken(tokenS string) (string, error) {
-
-	fmt.Printf("token not valid %s \n", tokenS)
-	token, err := jwt.Parse(tokenS, func(token *jwt.Token) (interface{}, error) {
-
-		//Make sure that the token method conform to "SigningMethodHMAC"
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-
-		return string([]byte("my_token_pass")), nil
-	})
-
-	if err != nil {
-		return "errr", err
-	}
-
-	if !token.Valid {
-		fmt.Println("token not valid")
-	}
-
-	return "", nil
-}
-
-/*func parseToken(t string) error {
-	token, err := jwt.Parse(t, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("There was an error")
-		}
-		return []byte("secret"), nil
-	})
-
-	if err != nil {
-		fmt.Printf("parce token err %s", err.Error())
-		return err
-	}
-
-	if !token.Valid {
-		fmt.Println("token not valid")
-	}
-	return err
-}
-*/
 
 func userLoggedIn(token string) bool {
 	t, err := parseToken(token)
@@ -352,18 +111,6 @@ func readConfig() Config {
 	return conf
 }
 
-func genToken(user *User) string {
-	t := &Token{UserId: 15}
-	t.ExpiresAt = time.Now().Add(2 * time.Second).Unix()
-	t.Subject = user.Name
-	t.Issuer = "golendar"
-	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), t)
-	tokenS, _ := token.SignedString([]byte("my_token_pass"))
-	user.Token = tokenS
-
-	return tokenS
-}
-
 func emptyIf(i interface{}) interface{} {
 
 	/*
@@ -398,45 +145,271 @@ func emptyIf(i interface{}) interface{} {
 }
 
 func main() {
-	//f := factory_.Factory("DB")
-	//f.WriteTo("hollo")
-	user := User{Name: "Andy", Pass: "123123", Token: ""}
+	/*user := User{Name: "Andy", Pass: "123123", Token: ""}
 	users = append(users, user)
 
 	t := genToken(&user)
-	fmt.Println("token: ", t)
+	fmt.Println("token: ", t)*/
 	intiLog()
-
 	conf := readConfig()
+	r := registerHandlers()
 	srv := &http.Server{
-		Handler: registerHandlers(),
+		Handler: r,
 		Addr:    host + ":" + strconv.Itoa(conf.Port),
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
+
 	log.Fatal(srv.ListenAndServe())
+}
+
+func registerHandlers() *mux.Router {
+	r := mux.NewRouter()
+
+	//user PUT
+	r.HandleFunc("/login", loginHandler)
+	r.HandleFunc("/logout", auth(logoutHandler))
+
+	//user PUT
+	r.HandleFunc("/api/user", auth(userHandler))
+
+	//user PUT
+	r.HandleFunc("/api/events", auth(eventsHandler))
+
+	//events GET PUT POST
+	r.HandleFunc("/api/event", auth(getEventHandler)).Methods(http.MethodGet)
+	r.HandleFunc("/api/event/{id}", auth(postEventHandler)).Methods(http.MethodPost)
+	r.HandleFunc("/api/event/{id}", auth(putEventHandler)).Methods(http.MethodPut)
+
+	return r
+}
+
+func parseReq(req io.Reader, v jsonPersarer) error {
+	body, err := ioutil.ReadAll(req)
+	if err != nil {
+		return err
+	}
+
+	err = v.Parse(body)
+	return err
+}
+
+func eventsHandler(w http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	e := Event{}
+	err = json.Unmarshal(body, &e)
+	if err != nil {
+		fmt.Fprintf(w, "Unmarshal err %s \n", err.Error())
+		return
+	}
+
+	fmt.Println(string(body))
+
+	if !userLoggedIn(e.Name) {
+		fmt.Fprintf(w, "User was not logged in %s \n", e.Name)
+		return
+	}
+
+	_, found := events[e.Id]
+	if found {
+		events[e.Id] = e
+	}
+
+	return
+}
+
+func getEventHandler(w http.ResponseWriter, req *http.Request) {
+	var eventId id
+	parseReq(req.Body, &eventId)
+
+	eVents := make(map[string]Event)
+
+	for k, v := range events {
+		if v.Id == string(eventId) {
+			eVents[k] = v
+		}
+	}
+
+	fmt.Fprintf(w, "Num of events {%d}  \n", len(eVents))
+}
+
+func postEventHandler(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func putEventHandler(w http.ResponseWriter, request *http.Request) {
+
+	c := context.WithValue(request.Context(), "id", "user_id")
+
+	request.WithContext(c)
+
+	//context.WithTimeout(c, (2 * time.Duration())
+	//updateDB(c, data))
+	v := request.Context().Value("w")
+	if v != nil {
+
+		s := v.(string)
+		fmt.Println(s)
+	}
+
+}
+
+func logoutHandler(w http.ResponseWriter, req *http.Request) {
+	u := User{}
+	err := parseReq(req.Body, &u)
+	if err != nil {
+		fmt.Fprintf(w, "Bad data \n")
+	}
+
+	v := req.Context().Value("id")
+	if v != nil {
+
+		s := v.(string)
+		fmt.Println(s)
+	} else {
+
+		log.Println()
+	}
+
+	for idx, v := range users {
+		fmt.Println("idx", idx, "v name: ", v.Name, "uname: ", u.Name)
+		if v.Name == u.Name {
+			removeElement(users, idx)
+			fmt.Fprintf(w, "User {%s} logged out \n", u.Name)
+			return
+		}
+	}
+
+	fmt.Fprintf(w, "User {%s} was not logged in. \n", u.Name)
+	return
+}
+
+func loginHandler(w http.ResponseWriter, req *http.Request) {
+	u := User{}
+	parseReq(req.Body, &u)
+	fmt.Println(len(users))
+
+	u.Token = genToken(&u)
+	b, err := json.Marshal(u)
+	if err != nil {
+		fmt.Fprintf(w, "err %s \n", err.Error())
+		return
+	}
+
+	w.Header().Add("Content-Type", " application/json")
+	fmt.Fprintf(w, string(b))
+
+	//fmt.Fprintf(w, "User {%s} need to create an account firt. \n", u.Name)
+	return
+}
+
+func userHandler(w http.ResponseWriter, req *http.Request) {
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	u := User{}
+	json.Unmarshal(body, &u)
+
+	fmt.Println(string(body))
+
+	if !userLoggedIn(u.Name) {
+		fmt.Fprintf(w, "User {%s} not logged in. \n", u.Name)
+	}
+
+	//todo update events
+	return
 }
 
 //auth
 func auth(h http.HandlerFunc) http.HandlerFunc {
 
 	return func(writer http.ResponseWriter, request *http.Request) {
+
 		tokenHeader := request.Header.Get("Authorization") //get token
 		token := strings.Split(tokenHeader, " ")[1]
 		//logging
 		//to do recovery
+
+		c := context.WithValue(request.Context(), "id", "user_id")
+		request.WithContext(c)
 
 		//fmt.Printf("before http handler %s", token)
 		if userLoggedIn(token) {
 			fmt.Println(writer, "user not authorized")
 			return
 		}
-
 		//to do add  id user to context
 		h(writer, request)
 	}
-
 }
 
-//cource
+func genToken(user *User) string {
+	t := &Token{UserId: 15}
+	//t.ExpiresAt = time.Now().Add(72 * time.Hour).Unix()
+	t.Subject = user.Name
+	t.Issuer = "golendar"
+
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), t.StandardClaims)
+
+	tokenS, _ := token.SignedString([]byte(my_token_pass))
+
+	user.Token = tokenS
+
+	return tokenS
+}
+
+func parseToken(tokenS string) (string, error) {
+
+	fmt.Printf("token not valid %s \n", tokenS)
+	token, err := jwt.Parse(tokenS, func(token *jwt.Token) (interface{}, error) {
+
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return string([]byte(my_token_pass)), nil
+	})
+
+	if err != nil {
+		return "error: ", err
+	}
+
+	if !token.Valid {
+		fmt.Println("token not valid: ")
+	}
+
+	return "", nil
+}
+
+type jsonPersarer interface {
+	Parse(b []byte) error
+}
+
+func (e *Event) Parse(b []byte) error {
+
+	err := json.Unmarshal(b, e)
+	return err
+}
+
+func (e *User) Parse(b []byte) error {
+
+	err := json.Unmarshal(b, e)
+	return err
+}
+
+type id string
+
+func (i *id) Parse(b []byte) error {
+
+	err := json.Unmarshal(b, i)
+	return err
+}
